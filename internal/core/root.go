f2331a849ed34722e4401af06e1647ba0e086343
Jack Sullivan <jack@divergent.codes>
2023-06-26 00:27:57 -0700

    ignore: remove old comment

--- internal/core/root.go
+++ internal/core/root.go
@@ -1,4 +1,4 @@
 // Package core implements cross-functional features used across the application
 //
-// Includes default configuration settings, logging.
+// Includes default configuration settings.
 package core
